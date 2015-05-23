23727dd98ea5adfa37c29303e3e98cd3a5e94155
Michael Aldridge <aldridge.mac@gmail.com>
2015-05-22 22:58:16 -0500

    add foolproof translation output

--- translate.js
+++ translate.js
@@ -1,17 +1,6 @@
 function translate() {
     var name = document.getElementById("tForm").user.value;
     var text = document.getElementById("tForm").question.value;
-    message = text;
-    document.getElementById("interact").innerHTML = alpacify(message);
-}
-
-function alpacify(s) {
-    for(var i = 0; i < s.length; i++) {
-	s.replaceAt(i, String.fromCharCode(s.charCodeAt(i) + Math.floor((Math.random() * 1000) + 1)));
-    }
-    return s;
-}
-
-String.prototype.replaceAt = function(index, character) {
-    return this.substr(0, index) + character + this.substr(index+character.length);
+    message = "Unfortunately it has been detected that your network is not using IPv6.  Alpaca Address Translation (AAT) is not backwards compatible with IPv4 at this time.";
+    document.getElementById("interact").innerHTML = message;
 }
